d077ee7f2cfcf2917bf0ece16e23bf7f377e45f9
chiyoung.jeong <chiyoung.jeong@linecorp.com>
2026-04-21 18:59:47 +0900

    refactor: remove html package

--- packages/html/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@line/abc-def-eslint-config/base";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["dist"],
-  },
-  ...baseConfig,
-];
